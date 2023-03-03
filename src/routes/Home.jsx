ab21fbe0fe9eb660db91d137abf5fed96f11d557
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-02 20:35:33 -0500

    create basic app structure

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className='absolute text-primary-hover m-auto'>Home</div>
+      <div className='relative top-48 flex justify-center'>
+          <div className='flex gap-1'>
+            <nav>Navigation</nav>
+            <section>Feed</section>
+          </div>
+      </div>
+    </>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#1DA1F2",
+      "primary-hover": "#1B95E0",
+      "primary-disable": "#EBEEF0",
+      "primary-text": "#EBEEF0",
+      "secondary-text": "#EBEEF0",
+      error: "#EBEEF0",
+      background: "#EBEEF0",
+      "backgronud-2": "#EBEEF0",
+    },
     extend: {},
   },
   plugins: [],
