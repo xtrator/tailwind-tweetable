3df7678bfa5f1741524b4ee5e4f11360e0a4e6a7
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-03 09:18:18 -0500

    improve Profile page a little

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Feed from "../components/page/Feed";
 import Nav from "../components/page/Nav";
 import Tweets from "../components/tweet/Tweets";
+import avatarUrl from "../assets/images/avatar.png";
 
 export default function Profile() {
   return (
@@ -11,15 +11,31 @@ export default function Profile() {
         <div className="flex">
           <Nav />
           <div className="bg-white border border-background-2 2xl:w-[800px] sm:w-[600px]">
-            <h2 className="p-4 text-2xl font-semibold text-center border border-background-2">
-              Profile
-            </h2>
             <div>
-              <p>Avatar Card</p>
+              <h2 className="p-4 text-2xl font-semibold text-center border border-background-2">
+                Profile
+              </h2>
             </div>
-            <div>
-              <p>Tweets and likes</p>
+            <div className="flex flex-col items-center border-1 border-background p-2">
+              <img className="w-24" src={avatarUrl} />
+              <p className="text-xl font-light">Diego Torres</p>
+              <p className="text-sm text-secondary-text">@diego</p>
+              <p className="text-base text-secondary-text">
+                CAL Joined on January 13, 2020
+              </p>
+              <button className=" font-light hover:text-primary-hover text-primary hover:border-primary-hover border-primary border rounded-full py-1 px-4 mt-2">
+                Edit Profile
+              </button>
+              <div className="flex w-full border-px border-background">
+                <p className="text-xl font-semi hover:text-primary w-full text-center">
+                  Tweets
+                </p>
+                <p className="text-xl font-semi hover:text-primary w-full text-center">
+                  Likes
+                </p>
+              </div>
             </div>
+
             <Tweets />
           </div>
         </div>
